4ee90deed715fc1ce396b86b43ae8438aa88bb06
Shadowchamber <shadowchamber@icloud.com>
2025-12-14 01:38:11 +0200

    remove py

--- old/orchestration/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Orchestrators for high-level lab workflows."""
-from .gluster import setup_glusterfs  # noqa: F401
-from .kubernetes import deploy_kubernetes  # noqa: F401
-__all__ = ["setup_glusterfs", "deploy_kubernetes"]
--- old/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services module - provides persistent connections and service wrappers
-"""
-from .lxc import LXCService
-from .pct import PCTService
-from .ssh import SSHService
-from .apt import APTService
-from .template import TemplateService
-__all__ = ["LXCService", "PCTService", "SSHService", "APTService", "TemplateService"]
